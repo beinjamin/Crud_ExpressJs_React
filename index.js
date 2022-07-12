f64b37f20f420c63462387fb9e864ad2bda0c9c1
Jaures <jauresbeinajmin@gmail.com>
2022-07-12 21:11:33 +0100

    Configuration du server

--- index.js
+++ index.js
@@ -9,4 +9,7 @@ const port = 3000;
 app,use(bordyParser.json());
 app,use(cors());
 
+app,get("/",(req,res)=> res,send ("Hello From Express"));
+app,listen (port , () => console.log ('server is listening on port: http://localhost:${port}')
 
+);
